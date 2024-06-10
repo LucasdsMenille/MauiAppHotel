c5ba4eb45917a2eeb2b9c6fd874234835e95888d
Usuario <Usuario@CPD03-M13>
2024-06-10 15:02:14 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem1.xaml.cs
@@ -0,0 +1,19 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem1 : ContentPage
+{
+	public ContratacaoHospedagem1()
+	{
+		InitializeComponent();
+	}
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ MauiAppHotel/Views/HospedagemContratada1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratada1 : ContentPage
+{
+	public HospedagemContratada1()
+	{
+		InitializeComponent();
+	}
+}
